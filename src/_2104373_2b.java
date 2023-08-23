44020aa1099d611e1af0050807ee6f4241f6752e
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-23 23:55:32 +0530

    Initial commit

---
+++ src/_2104373_2b.java
@@ -0,0 +1,33 @@
+public class _2104373_2b {
+    public static void main(String[] args) {
+        int X = 100; // Change X to the desired value
+
+        int maxShekelsFlicked = calculateMaxShekelsFlicked(X);
+        System.out.println("Maximum shekels flicked: " + maxShekelsFlicked);
+    }
+
+    public static int calculateMaxShekelsFlicked(int X) {
+        int maxShekels = 0;
+        for (int i = 1; i <= X; i++) {
+            int length = computeSequenceLength(i);
+            maxShekels += length;
+        }
+        return maxShekels;
+    }
+
+    public static int computeSequenceLength(int num) {
+        int length = 0;
+
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num + num * 2 + 1;
+            }
+
+            length++;
+        }
+
+        return length;
+    }
+}
